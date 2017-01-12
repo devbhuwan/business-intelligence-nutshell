4c04c6783ebc10206454db6c04cbd2cba4ccf99d
Bhuwan Upadhyay <comp.er.bhuwan@gmail.com>
2017-01-12 16:10:30 +0300

    bi-jboss-dashbuilder

---
+++ jboss-dashbuilder/src/main/java/io/github/devbhuwan/JbossDashbuilderApplication.java
@@ -0,0 +1,12 @@
+package io.github.devbhuwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbossDashbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbossDashbuilderApplication.class, args);
+	}
+}
